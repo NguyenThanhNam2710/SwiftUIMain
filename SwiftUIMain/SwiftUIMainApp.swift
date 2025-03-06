0ec3ec3ddb21e9552e6dd9ceaaefa6ab35a5a943
NamNT97 <thanhnam2k27@gmail.com>
2025-03-06 17:08:27 +0700

    Initial Commit

---
+++ SwiftUIMain/SwiftUIMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMainApp.swift
+//  SwiftUIMain
+//
+//  Created by NamNT97 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
